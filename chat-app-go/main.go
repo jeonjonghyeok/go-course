7c00ab8051a228670984280ad558ecde01ffbe40
jeonjonghyeok <tongher1685@gmail.com>
2021-03-02 02:04:04 +0900

    go main

--- chat-app-go/main.go
+++ chat-app-go/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/jeonjonghyeok/go-run/chat-app-go/ws"
 )
 
 func main() {
-	if err := http.ListenAndServe(":5000", messageHandler()); err != nil {
+	if err := http.ListenAndServe(":5000", ws.MessageHandler()); err != nil {
 		log.Fatalln(err)
 	}
 }
