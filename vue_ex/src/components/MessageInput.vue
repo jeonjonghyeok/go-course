b371d46c0f9cf5b94ff1f4555290dff3ba0e1e7a
jeonjonghyeok <tongher1685@gmail.com>
2021-03-02 01:01:33 +0900

    vue message component

--- vue_ex/src/components/MessageInput.vue
+++ vue_ex/src/components/MessageInput.vue
@@ -1,9 +1,26 @@
 <template>
+<form
+   @submit.prevent="sendMessage"
+   >
+        <input type="text" v-model="message" placeholder="아무말이나 입력하세요"/>
+        <input type="submit" value="보내기"/>
+    </form>
 </template>
 
 <script>
+import { ref } from "vue"
 export default {
-    name: 'MessageInput'
+    name: 'MessageInput',
+    setup(props,{ emit }) {
+        const message = ref("")
+        const sendMessage = () => {
+            emit("send",message.value.trim())
+        }
+        return { 
+            message,
+            sendMessage
+        }
+    }
 }
 </script>
 
--- vue_ex/src/components/Messenger.vue
+++ vue_ex/src/components/Messenger.vue
@@ -1,16 +1,37 @@
 <template>
-    <div>
-        Some Componens
+    <div v-for="(message, idx) in messages" 
+    :key="idx">
+        {{message}}
     </div>
+    <MessageInput @send="sendMessage" />
 </template>
 
 <script>
+import MessageInput from '@/components/MessageInput.vue'
+import { ref } from 'vue'
 export default {
     name: 'Messenger',
     components: {
+        MessageInput
     },
     setup() {
+        const socket = new WebSocket('ws://localhost:5000');
+        const message = ref("")
+        const messages = ref([])
+        const sendMessage = () => {
+            socket.send(message)
+        }
+        socket.onmessage = (message) => {
+            messages.value.push(message.data)
+        }
+        
+        
+
         return {
+            message,
+            messages,
+            sendMessage
+
         }
     }
 }
